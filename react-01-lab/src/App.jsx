3ceee1b75a35c3e891e6480c5d515f2ffea204d9
Azizfalshammari <azizfalshammari.com>
2024-06-09 19:15:10 +0300

    done

--- react-01-lab/src/App.jsx
+++ react-01-lab/src/App.jsx
@@ -7,7 +7,39 @@ function App() {
   return (
     <>
     <Companies
+    companyName = 'aramco'
+    service = 'oilgas'
+    price = '1m$'
+    worker= '1000'
+    found = '2009'
+    name1  = 'nouf' 
+    langs1 = 'JS, JSX, HTML!'
+    exp1 = '5'
+    name2  = 'fahad' 
+    langs2 = 'JS, JSX, HTML!'
+    exp2 = '5'
+    name3  = 'demah' 
+    langs3 = 'JS, JSX, HTML!'
+    exp3 = '5'
+    />
+        <Companies
     companyName = 'Tuwaiq'
+    service = 'oilgas'
+    price = '1m$'
+    worker= '1000'
+    found = '2009'
+    name1  = 'nouf' 
+    langs1 = 'JS, JSX, HTML!'
+    exp1 = '5'
+    name2  = 'fahad' 
+    langs2 = 'JS, JSX, HTML!'
+    exp2 = '5'
+    name3  = 'demah' 
+    langs3 = 'JS, JSX, HTML!'
+    exp3 = '5'
+    />
+        <Companies
+    companyName = 'aramco'
     service = 'oilgas'
     price = '1m$'
     worker= '1000'
